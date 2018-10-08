6191d525eaf838e511194dc8c6c6f191c6633966
Laura Darlak <laura@polyporedesign.com>
2018-10-08 22:20:37 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,7 @@ def select_series_title_with_most_human_characters
    FROM series
    INNER JOIN characters ON series.id = character.series.id
    ORDER BY COUNT(character.species) DESC LIMIT 1
-   WHERE character.species = 'human';"
+   HAVING character.species = 'human';"
 end
 
 def select_character_names_and_number_of_books_they_are_in
