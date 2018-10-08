1b91b04506d014a723cb5fa93d9e9ec3c96d6802
Laura Darlak <laura@polyporedesign.com>
2018-10-08 22:29:40 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -38,5 +38,5 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(character_books.character_id)
    FROM characters
    INNER JOIN character_books ON character_books.character_id = characters.id
-   GROUP BY characters.name"
+   GROUP BY characters.name;"
 end
