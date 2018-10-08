738760a9d3e10df7605949d6711bf20fa35bb709
Laura Darlak <laura@polyporedesign.com>
2018-10-08 22:33:45 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,5 +39,5 @@ def select_character_names_and_number_of_books_they_are_in
    FROM characters
    INNER JOIN character_books ON character_books.character_id = characters.id
    GROUP BY(characters.name)
-   ORDER BY COUNT(character_books.character_id);"
+   ORDER BY COUNT(character_books.character_id) DESC;"
 end
